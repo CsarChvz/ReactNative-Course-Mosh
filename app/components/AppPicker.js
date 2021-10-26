0eee6a14b49c3cbfd9d2aa05a5e8840545b355a5
CsarChvz <cesarchavezrodriguez9@gmail.com>
2021-10-26 17:05:48 -0500

    Modal cuando se presiona en el appPicker

--- app/components/AppPicker.js
+++ app/components/AppPicker.js
@@ -1,29 +1,53 @@
-import React from "react";
-import { View, StyleSheet, TextInput, Platform } from "react-native";
+import React, { useState } from "react";
+import {
+  View,
+  StyleSheet,
+  TextInput,
+  Platform,
+  TouchableWithoutFeedback,
+  Modal,
+  Button,
+} from "react-native";
 import { MaterialCommunityIcons } from "@expo/vector-icons";
 
+import Screen from "./Screen";
 import AppText from "../components/AppText";
 
 import colors from "../config/colors";
 import stylesGlobal from "../config/stylesGlobal";
 function AppPicker({ icon, placeholder, ...others }) {
+  const [modalVisible, setmodalVisible] = useState(false);
   return (
-    <View style={styles.container}>
-      {icon && (
-        <MaterialCommunityIcons
-          name={icon}
-          size={20}
-          color={colors.medium}
-          style={styles.icon}
-        />
-      )}
-      <AppText style={styles.text}>{placeholder}</AppText>
-      <MaterialCommunityIcons
-        name="chevron-down"
-        size={20}
-        color={colors.medium}
-      />
-    </View>
+    <>
+      <TouchableWithoutFeedback
+        onPress={() => {
+          setmodalVisible(true);
+        }}
+      >
+        <View style={styles.container}>
+          {icon && (
+            <MaterialCommunityIcons
+              name={icon}
+              size={20}
+              color={colors.medium}
+              style={styles.icon}
+            />
+          )}
+          <AppText style={styles.text}>{placeholder}</AppText>
+          <MaterialCommunityIcons
+            name="chevron-down"
+            size={20}
+            color={colors.medium}
+          />
+        </View>
+      </TouchableWithoutFeedback>
+
+      <Modal visible={modalVisible} animationType="slide">
+        <Screen>
+          <Button title="close" onPress={() => setmodalVisible(false)} />
+        </Screen>
+      </Modal>
+    </>
   );
 }
 const styles = StyleSheet.create({
